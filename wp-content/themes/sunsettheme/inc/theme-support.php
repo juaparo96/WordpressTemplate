d8c7dbb694f2eda54593083e904ae4fe02db519d
Juan <romerohm1996@gmail.com>
2018-02-09 15:56:12 -0500

    leccion 19,29,21,22
    creacion de nuevos archivos sass,
    archivo de contenido de post,

--- wp-content/themes/sunsettheme/inc/theme-support.php
+++ wp-content/themes/sunsettheme/inc/theme-support.php
@@ -53,17 +53,32 @@ function sunset_posted_meta () {
   if (!empty($categories) ):
     foreach ($categories as $category ) :
 
-    if($i > 1): $output .= $separator; endif;
+      if($i > 1): $output .= $separator; endif;
 
       $output .= '<a href="'. esc_url( get_category_link($category->term_id) ).'" alt="'.esc_attr('View all posts in%s'
       , $category->name).'">'.esc_html($category->name).'</a> ';
-    $i++;  endforeach;
-  endif;
-  return '<span class="posted-on"> <a href="'.esc_url(get_permalink() ).'"> '. $posted_on .'</a> ago </span> / <span
-  class class="posted-in">'.$output.'</span>';
-}
+      $i++;  endforeach;
+    endif;
+    return '<span class="posted-on"> <a href="'.esc_url(get_permalink() ).'"> '. $posted_on .'</a> ago </span> / <span
+    class class="posted-in">'.$output.'</span>';
+  }
+  //Obtener tags y numero de comentarios de un post
+  function sunset_posted_footer() {
 
-function sunset_posted_footer() {
-  return '<div class="post-footer-container"><div class="row"><div class"col-xs-12 col-sm-6">'. get_the_tag_list()
-  .'</div><div class="col-xs-12 col-sm-6">'.$comments.'</div></div></div>';
-}
+    $comments_num = get_comments_number();
+    if( comments_open() ){
+      if( $comments_num == 0 ){
+        $comments = __('No Comments');
+      } elseif ( $comments_num > 1 ){
+        $comments= $comments_num . __(' Comments');
+      } else {
+        $comments = __('1 Comment');
+      }
+      $comments = '<a class="comments-link"  href="' . get_comments_link() . '">'. $comments .' <span class="sunset-icon sunset-comment"></span></a>';
+    } else {
+      $comments = __('Comments are closed');
+    }
+
+    return '<div class="post-footer-container"><div class="row"><div class="col-xs-12 col-sm-6">'. get_the_tag_list('<div class="tags-list"><span class="sunset-icon sunset-tag"></span>', ' ', '</div>') .'</div>
+    <div class="col-xs-12 col-sm-6 text-right">'. $comments .'</div></div></div>';
+  }
