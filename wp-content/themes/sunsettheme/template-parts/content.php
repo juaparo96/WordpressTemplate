fc3d4c716d318e54bfa9df200bbee46a2da0382e
Juan <romerohm1996@gmail.com>
2018-02-09 15:56:56 -0500

    Revert "leccion 19,29,21,22"
    This reverts commit d8c7dbb694f2eda54593083e904ae4fe02db519d.

--- wp-content/themes/sunsettheme/template-parts/content.php
+++ wp-content/themes/sunsettheme/template-parts/content.php
@@ -8,9 +8,9 @@
 ?>
 
 <article id="post-<?php  the_ID(); ?>" <?php post_class(); ?> >
-  <header class="entry-header text-center">
+  <header class="entry-header">
 
-    <?php the_title('<h1 class="entry-title"><a href="'. esc_url( get_permalink() ) .'" rel="bookmark">', '</a></h1>'); ?>
+    <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
 
     <div class="entry-meta">
       <?php echo sunset_posted_meta(); ?>
@@ -20,12 +20,10 @@
 
   <div class="entry-content">
 
-    <?php if ( has_post_thumbnail() ) :
-      $featured_image = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) );
-      ?>
+    <?php if ( has_post_thumbnail() ) : ?>
 
-      <a class="standart-featured-link" href="<?php the_permalink(); ?>"></a>
-      <div class="standart-featured background-image" style="background-image: url(<?php echo $featured_image; ?>)">
+      <div class="standart-featured">
+        <?php the_post_thumbnail(); ?>
       </div>
 
       <?php endif; ?>
@@ -34,8 +32,8 @@
         <?php the_excerpt(); ?>
       </div>
 
-      <div class="button-container text-center">
-          <a href="<?php the_permalink(); ?>" class="btn btn-sunset"><?php _e('Read More'); ?></a>
+      <div class="button-container">
+          <a href="<?php the_permalink(); ?>" class="btn btn-default"><?php _e('Read More'); ?></a>
       </div>
 
   </div> <!-- .entry-content -->
