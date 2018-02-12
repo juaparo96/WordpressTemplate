9438731945a04244f8c7ea85bb6edbc367cb8e42
Juan <romerohm1996@gmail.com>
2018-02-12 16:56:00 -0500

    lecciones 21 - 25
    nuevos archivos:
    post video, post de audio.
    nuevas funciones en theme-support

--- wp-content/themes/sunsettheme/inc/theme-support.php
+++ wp-content/themes/sunsettheme/inc/theme-support.php
@@ -53,17 +53,77 @@ function sunset_posted_meta () {
   if (!empty($categories) ):
     foreach ($categories as $category ) :
 
-    if($i > 1): $output .= $separator; endif;
+      if($i > 1): $output .= $separator; endif;
 
       $output .= '<a href="'. esc_url( get_category_link($category->term_id) ).'" alt="'.esc_attr('View all posts in%s'
       , $category->name).'">'.esc_html($category->name).'</a> ';
-    $i++;  endforeach;
-  endif;
-  return '<span class="posted-on"> <a href="'.esc_url(get_permalink() ).'"> '. $posted_on .'</a> ago </span> / <span
-  class class="posted-in">'.$output.'</span>';
-}
+      $i++;  endforeach;
+    endif;
+    return '<span class="posted-on"> <a href="'.esc_url(get_permalink() ).'"> '. $posted_on .'</a> ago </span> / <span
+    class class="posted-in">'.$output.'</span>';
+  }
 
-function sunset_posted_footer() {
-  return '<div class="post-footer-container"><div class="row"><div class"col-xs-12 col-sm-6">'. get_the_tag_list()
-  .'</div><div class="col-xs-12 col-sm-6">'.$comments.'</div></div></div>';
+  function sunset_posted_footer() {
+
+    $comments_num = get_comments_number ();
+    if(comments_open() ){
+      //Obtener link de comentarios
+      if ($comments_num == 0 ) {
+        $comments = __('No Comments');
+      } elseif ($comments_num > 1) {
+        $comments = $comments_num . __('Comments');
+      } else {
+        $comments = __('1 Comment');
+      }
+      $comments = '<a class="comments-link" href="'. get_comments_link().'">'.$comments.'<span class="sunset-icon sunset-comment"></span></a>';
+    } else {
+      $comments = __('Comments are closed');
+    }
+    return '<div class="post-footer-container"><div class="row"><div class="col-xs-12 col-sm-6">'. get_the_tag_list('<div class="tags-list"><span class="sunset-icon sunset-tag"></span>', ' ', '</div>') .'</div><div class="col-xs-12 col-sm-6 text-right">'. $comments .'</div></div></div>';
+
+  }
+
+
+/*
+      Obtener imagenes del post
+ */
+  function sunset_get_attachment() {
+    $output = '';
+    if ( has_post_thumbnail() ) :
+      $output = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) );
+    else:
+      $attachments = get_posts( array(
+        'post_type' => 'attachment',
+        'posts_per_page' => 1,
+        'post_parent' => get_the_ID()
+
+      ) );
+      if ($attachments):
+        foreach ($attachments as $attachment) :
+          $output = wp_get_attachment_url($attachment->ID);
+        endforeach;
+      endif;
+      wp_reset_postdata();
+    endif;
+
+    return $output;
+  }
+
+/*
+
+  Obtener audio
+ */
+
+function sunset_get_embedded_media($type = array()) {
+
+  $content = do_shortcode(apply_filters('the_content', get_the_content())  );
+  $embed = get_media_embedded_in_content($content, $type);
+
+  if (in_array('audio', $type) ):
+  $output =  str_replace('?visual=true', '?visual=false' , $embed[0]);
+else:
+  $output = $embed[0];
+endif;
+  return  $output;
+
 }
--- wp-content/themes/sunsettheme/template-parts/content-video.php
+++ wp-content/themes/sunsettheme/template-parts/content-video.php
@@ -3,12 +3,15 @@
 /*
 
 @package
--- Formato estandar delpost
+-- Formato de Post en Video
 */
 ?>
 
-<article id="post-<?php  the_ID(); ?>" <?php post_class(); ?> >
+<article id="post-<?php  the_ID(); ?>" <?php post_class('sunset-format-video'); ?> >
+
   <header class="entry-header text-center">
+
+    <div class="embed-responsive embed-responsive-16by9"><?php echo sunset_get_embedded_media(array('video', 'iframe'));?></div>
 
     <?php the_title('<h1 class="entry-title"><a href="'. esc_url(get_permalink()  ) .'" rel="bookmark">', '</a></h1>'); ?>
 
